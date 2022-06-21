c4781b5961426d046cfce62bc6765fe0180181f6
Darshan Bhangale <f20190800@goa.bits-pilani.ac.in>
2022-06-22 01:12:56 +0530

    done

--- add_date.py
+++ add_date.py
@@ -1,11 +1,12 @@
 import datetime
 import sublime, sublime_plugin
- 
+f=open("C:/Users/darsh/AppData/Roaming/Sublime Text 3/Packages/User/qqqtxt.txt","r") 
+contents=f.read()
 class TimestampCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     timestamp = "[%s]\t" % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-    self.view.insert(edit, self.view.sel()[0].begin(),"/**\n *      author: Darshan_bhangale\n *      created: "+timestamp+"\n**/")
+    self.view.insert(edit, self.view.sel()[0].begin(),"/**\n *      author: Darshan_Bhangale\n *      created: "+timestamp+"\n**/\n"+contents)
 
-
+# f.close()
 # "/**\n*\t\t\tauthor:  Darshan_Bhangale\n*\t\t\tcreated: +"timestamp"+\n       **/"
 # "/**\n*\t\t\tauthor:  Darshan_Bhangale\n*\t\t\tcreated: +"timestamp"+\n       **/"
